d959a8e8cdbc4644effa0b5600e135552440b29b
Harsh Panchal <harshpanchal7979@gmail.com>
2026-04-01 11:24:19 +0530

    remove unique constraint on users.email to allow inactive users to re-register

--- revurge.sql
+++ revurge.sql
@@ -808,3 +808,8 @@ COLLATE=utf8mb4_general_ci;
 -- 30-03-26
 
 ALTER TABLE `sales_quotes` MODIFY COLUMN `status` ENUM('draft','pending','sent','viewed','accepted','rejected','expired') NOT NULL DEFAULT 'draft';
+
+-- 01-04-26
+
+-- IGNORE ERR IF INDEX DOES NOT EXISTS
+ALTER TABLE users DROP INDEX email;
