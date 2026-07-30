fb4f821fa9f6e6a4e46265739e21e04f4901b0b9
Harsh Panchal <harshpanchal7979@gmail.com>
2026-07-30 10:27:31 +0530

    fix query

--- revurge.sql
+++ revurge.sql
@@ -3246,96 +3246,124 @@ CROSS JOIN (
 ) images
 WHERE s.slug = 'beige-west-hollywood-content-studio';
 
-INSERT INTO studio_media (studio_id, media_type, url, thumbnail_url, title, alt_text, sort_order, is_cover, metadata)
-SELECT s.studio_id, 'image', CONCAT('https://d2jhn32fsulyac.cloudfront.net/assets/studio/', image_path), NULL, s.studio_name, s.studio_name, ord - 1, IF(ord = 1, 1, 0), NULL
+INSERT INTO studio_media (
+    studio_id,
+    media_type,
+    url,
+    thumbnail_url,
+    title,
+    alt_text,
+    sort_order,
+    is_cover,
+    metadata
+)
+SELECT
+    s.studio_id,
+    'image',
+    CONCAT('https://d2jhn32fsulyac.cloudfront.net/assets/studio/', i.image_path),
+    NULL,
+    s.studio_name,
+    s.studio_name,
+    i.ord - 1,
+    IF(i.ord = 1, 1, 0),
+    NULL
 FROM studios s
 CROSS JOIN (
-  SELECT 1 AS ord, 'weho-gym/Copy+of+DSC00042.jpg' AS image_path
-  UNION ALL SELECT 2, 'weho-gym/Copy+of+DSC00056.jpg'
-  UNION ALL SELECT 3, 'weho-gym/Copy+of+IMG_1280.jpg'
-  UNION ALL SELECT 4, 'weho-gym/Copy+of+IMG_7584.jpg'
-  UNION ALL SELECT 5, 'weho-gym/Copy+of+IMG_7595.jpg'
-  UNION ALL SELECT n + 5, CONCAT('weho-gym/MWC+Weho+Studio-', n, '.jpg')
-  FROM (
-    SELECT 1 AS n
-    UNION ALL SELECT 2
-    UNION ALL SELECT 3
-    UNION ALL SELECT 4
-    UNION ALL SELECT 5
-    UNION ALL SELECT 6
-    UNION ALL SELECT 7
-    UNION ALL SELECT 8
-    UNION ALL SELECT 9
-    UNION ALL SELECT 10
-    UNION ALL SELECT 11
-    UNION ALL SELECT 12
-    UNION ALL SELECT 13
-    UNION ALL SELECT 14
-    UNION ALL SELECT 15
-    UNION ALL SELECT 16
-    UNION ALL SELECT 17
-    UNION ALL SELECT 18
-    UNION ALL SELECT 19
-    UNION ALL SELECT 20
-    UNION ALL SELECT 21
-    UNION ALL SELECT 22
-    UNION ALL SELECT 23
-    UNION ALL SELECT 24
-    UNION ALL SELECT 25
-    UNION ALL SELECT 26
-    UNION ALL SELECT 27
-    UNION ALL SELECT 28
-    UNION ALL SELECT 29
-    UNION ALL SELECT 30
-    UNION ALL SELECT 31
-    UNION ALL SELECT 32
-    UNION ALL SELECT 33
-    UNION ALL SELECT 34
-    UNION ALL SELECT 35
-    UNION ALL SELECT 36
-    UNION ALL SELECT 37
-    UNION ALL SELECT 38
-    UNION ALL SELECT 39
-    UNION ALL SELECT 40
-    UNION ALL SELECT 41
-    UNION ALL SELECT 42
-    UNION ALL SELECT 43
-    UNION ALL SELECT 44
-    UNION ALL SELECT 45
-    UNION ALL SELECT 46
-    UNION ALL SELECT 47
-    UNION ALL SELECT 48
-    UNION ALL SELECT 49
-    UNION ALL SELECT 50
-    UNION ALL SELECT 51
-    UNION ALL SELECT 52
-    UNION ALL SELECT 53
-    UNION ALL SELECT 54
-    UNION ALL SELECT 55
-    UNION ALL SELECT 56
-    UNION ALL SELECT 57
-    UNION ALL SELECT 58
-    UNION ALL SELECT 59
-    UNION ALL SELECT 60
-    UNION ALL SELECT 61
-    UNION ALL SELECT 62
-    UNION ALL SELECT 63
-    UNION ALL SELECT 64
-    UNION ALL SELECT 65
-    UNION ALL SELECT 66
-    UNION ALL SELECT 67
-    UNION ALL SELECT 68
-    UNION ALL SELECT 69
-    UNION ALL SELECT 70
-    UNION ALL SELECT 71
-    UNION ALL SELECT 72
-    UNION ALL SELECT 73
-    UNION ALL SELECT 74
-    UNION ALL SELECT 75
-    UNION ALL SELECT 76
-    UNION ALL SELECT 77
-  ) generated
-) images
+    SELECT 1 AS ord, 'weho-gym/Copy+of+DSC00042.jpg' AS image_path
+    UNION ALL
+    SELECT 2, 'weho-gym/Copy+of+DSC00056.jpg'
+    UNION ALL
+    SELECT 3, 'weho-gym/Copy+of+IMG_1280.jpg'
+    UNION ALL
+    SELECT 4, 'weho-gym/Copy+of+IMG_7584.jpg'
+    UNION ALL
+    SELECT 5, 'weho-gym/Copy+of+IMG_7595.jpg'
+
+    UNION ALL
+
+    SELECT
+        g.n + 5,
+        CONCAT('weho-gym/MWC+Weho+Studio-', g.n, '.jpg')
+    FROM (
+        SELECT 1 AS n
+        UNION ALL SELECT 2
+        UNION ALL SELECT 3
+        UNION ALL SELECT 4
+        UNION ALL SELECT 5
+        UNION ALL SELECT 6
+        UNION ALL SELECT 7
+        UNION ALL SELECT 8
+        UNION ALL SELECT 9
+        UNION ALL SELECT 10
+        UNION ALL SELECT 11
+        UNION ALL SELECT 12
+        UNION ALL SELECT 13
+        UNION ALL SELECT 14
+        UNION ALL SELECT 15
+        UNION ALL SELECT 16
+        UNION ALL SELECT 17
+        UNION ALL SELECT 18
+        UNION ALL SELECT 19
+        UNION ALL SELECT 20
+        UNION ALL SELECT 21
+        UNION ALL SELECT 22
+        UNION ALL SELECT 23
+        UNION ALL SELECT 24
+        UNION ALL SELECT 25
+        UNION ALL SELECT 26
+        UNION ALL SELECT 27
+        UNION ALL SELECT 28
+        UNION ALL SELECT 29
+        UNION ALL SELECT 30
+        UNION ALL SELECT 31
+        UNION ALL SELECT 32
+        UNION ALL SELECT 33
+        UNION ALL SELECT 34
+        UNION ALL SELECT 35
+        UNION ALL SELECT 36
+        UNION ALL SELECT 37
+        UNION ALL SELECT 38
+        UNION ALL SELECT 39
+        UNION ALL SELECT 40
+        UNION ALL SELECT 41
+        UNION ALL SELECT 42
+        UNION ALL SELECT 43
+        UNION ALL SELECT 44
+        UNION ALL SELECT 45
+        UNION ALL SELECT 46
+        UNION ALL SELECT 47
+        UNION ALL SELECT 48
+        UNION ALL SELECT 49
+        UNION ALL SELECT 50
+        UNION ALL SELECT 51
+        UNION ALL SELECT 52
+        UNION ALL SELECT 53
+        UNION ALL SELECT 54
+        UNION ALL SELECT 55
+        UNION ALL SELECT 56
+        UNION ALL SELECT 57
+        UNION ALL SELECT 58
+        UNION ALL SELECT 59
+        UNION ALL SELECT 60
+        UNION ALL SELECT 61
+        UNION ALL SELECT 62
+        UNION ALL SELECT 63
+        UNION ALL SELECT 64
+        UNION ALL SELECT 65
+        UNION ALL SELECT 66
+        UNION ALL SELECT 67
+        UNION ALL SELECT 68
+        UNION ALL SELECT 69
+        UNION ALL SELECT 70
+        UNION ALL SELECT 71
+        UNION ALL SELECT 72
+        UNION ALL SELECT 73
+        UNION ALL SELECT 74
+        UNION ALL SELECT 75
+        UNION ALL SELECT 76
+        UNION ALL SELECT 77
+    ) g
+) i
 WHERE s.slug = 'beige-west-hollywood-wellness-gym';
 
 INSERT INTO studio_media (studio_id, media_type, url, thumbnail_url, title, alt_text, sort_order, is_cover, metadata)
