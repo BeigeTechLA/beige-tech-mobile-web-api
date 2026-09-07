dfc7fa2801cee5983e0496597a34905ceba5b042
Harsh Panchal <harshpanchal7979@gmail.com>
2026-09-07 18:24:40 +0530

    feat: enhance shift management permissions for create, edit, and delete operations

--- src/routes/shifts.routes.js
+++ src/routes/shifts.routes.js
@@ -9,6 +9,15 @@ const shiftManagementView = requireAnyPermission([
   'sales_admin_sales_people.view',
   'sales_rep_sales.view'
 ], { allowRoles: ['sales_rep', 'sales_admin'] });
+const shiftManagementCreate = requireAnyPermission([
+  'admin_sales_representative_shift_management.create',
+], { allowRoles: ['sales_rep', 'sales_admin'] });
+const shiftManagementEdit = requireAnyPermission([
+  'admin_sales_representative_shift_management.edit',
+], { allowRoles: ['sales_rep', 'sales_admin'] });
+const shiftManagementDelete = requireAnyPermission([
+  'admin_sales_representative_shift_management.delete',
+], { allowRoles: ['sales_rep', 'sales_admin'] });
 
 router.use(authenticate, shiftManagementView);
 
@@ -17,22 +26,22 @@ router.get('/overview', controller.overview);
 router.get('/hourly-lead-volume', controller.hourlyLeadVolume);
 router.get('/active-now', controller.activeNow);
 router.get('/recent-assignments', controller.recentAssignments);
-router.post('/', controller.createShift);
+router.post('/', shiftManagementCreate, controller.createShift);
 router.get('/', controller.listShifts);
 router.get('/salespeople', controller.listAllShiftSalespeople);
 router.get('/:id', controller.getShift);
-router.put('/:id', controller.updateShift);
-router.patch('/:id/toggle', controller.toggleShift);
-router.delete('/:id', controller.deleteShift);
+router.put('/:id', shiftManagementEdit, controller.updateShift);
+router.patch('/:id/toggle', shiftManagementEdit, controller.toggleShift);
+router.delete('/:id', shiftManagementDelete, controller.deleteShift);
 
 // Shift salespeople links to existing users/sales-reps data
-router.post('/:id/salespeople', controller.addSalesperson);
+router.post('/:id/salespeople', shiftManagementEdit, controller.addSalesperson);
 router.get('/:id/salespeople', controller.listSalespeople);
-router.patch('/:id/salespeople/:salesRepId/toggle', controller.toggleSalesperson);
-router.delete('/:id/salespeople/:salesRepId', controller.removeSalesperson);
+router.patch('/:id/salespeople/:salesRepId/toggle', shiftManagementEdit, controller.toggleSalesperson);
+router.delete('/:id/salespeople/:salesRepId', shiftManagementDelete, controller.removeSalesperson);
 
 // Round robin
 router.get('/:id/round-robin', controller.getRoundRobin);
-router.put('/:id/round-robin', controller.updateRoundRobin);
+router.put('/:id/round-robin', shiftManagementEdit, controller.updateRoundRobin);
 
 module.exports = router;
